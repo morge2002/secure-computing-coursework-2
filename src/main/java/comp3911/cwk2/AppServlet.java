43d30980f8369521b1fcc12d7863edafa1505cd7
a2z-adrian <43860371+a2z-adrian@users.noreply.github.com>
2023-12-12 23:46:10 +0000

    Remove console logging

--- src/main/java/comp3911/cwk2/AppServlet.java
+++ src/main/java/comp3911/cwk2/AppServlet.java
@@ -5,8 +5,12 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.*;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,8 +31,8 @@ import freemarker.template.TemplateExceptionHandler;
 public class AppServlet extends HttpServlet {
 
   private static final String CONNECTION_URL = "jdbc:sqlite:db.sqlite3";
-  private static final String PREPARED_AUTH_QUERY = "select * from user where username=? and password=?";
-  private static final String PREPARED_SEARCH_QUERY = "select * from patient where surname=? collate nocase";
+  private static final String AUTH_QUERY = "select * from user where username=? and password=?";
+  private static final String SEARCH_QUERY = "select * from patient where surname='%s' collate nocase";
 
   //The following are added to implement hashing passwords with a salt
   private static final String UPDATE_HASHES = "update user set password=?, salt=? where id=?";
@@ -68,15 +72,6 @@ public class AppServlet extends HttpServlet {
     }
   }
 
-  public static String encodeForHTML(String userInput) {
-    return userInput.replaceAll("&", "&amp;")
-            .replaceAll("<", "&lt;")
-            .replaceAll(">", "&gt;")
-            .replaceAll("\"", "&quot;")
-            .replaceAll("'", "&#x27;")
-            .replaceAll("/", "&#x2F;");
-  }
-
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
    throws ServletException, IOException {
@@ -95,9 +90,9 @@ public class AppServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
    throws ServletException, IOException {
     // Get form parameters
-     String username = encodeForHTML(request.getParameter("username"));
-     String password = encodeForHTML(request.getParameter("password"));
-     String surname = encodeForHTML(request.getParameter("surname"));
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    String surname = request.getParameter("surname");
 
     try {
       if (authenticated(username, password)) {
@@ -137,13 +132,11 @@ public class AppServlet extends HttpServlet {
 
     String salt = getSalt(username);
 
-    //Prepared statement version
     //Modified query to add salt to the password provided then search for the hashed version of that
     //Change required as a salted and triple hashed version of the password is now stored in the DB
-    try (PreparedStatement stmt = database.prepareStatement(PREPARED_AUTH_QUERY)) {
+    try (PreparedStatement stmt = database.prepareStatement(AUTH_QUERY)) {
       stmt.setString(1, username);
       stmt.setString(2, hashPassword(hashPassword(hashPassword(password + salt))));
-
       ResultSet results = stmt.executeQuery();
       return results.next();
     }
@@ -166,7 +159,6 @@ public class AppServlet extends HttpServlet {
           hexString.append(hex);
       }
 
-      System.out.println(hexString.toString());
     return hexString.toString();
     }
 
@@ -193,7 +185,6 @@ public class AppServlet extends HttpServlet {
           rec.setSalt(generateSalt());
 
           if (rec.getPassword().length() != 64){
-            System.out.println("Unhashed password detected: " + rec.getPassword());
             users.add(rec);
           }
       }
@@ -206,7 +197,6 @@ public class AppServlet extends HttpServlet {
           passwordStmt.setString(2, user.getSalt());
           passwordStmt.setString(3, user.getUserID());
 
-          System.out.println("Hashing password " + user.getPassword() + " to " +  hashPassword(user.getPassword()));
           passwordStmt.executeUpdate();
         }
       }
@@ -230,16 +220,14 @@ public class AppServlet extends HttpServlet {
     }
 
     String salt = sb.toString();
-    System.out.println(salt);
     return salt;
     }
 
   private List<Record> searchResults(String surname) throws SQLException {
     List<Record> records = new ArrayList<>();
-    // Prepared statement version
-    try (PreparedStatement stmt = database.prepareStatement(PREPARED_SEARCH_QUERY)) {
-      stmt.setString(1, surname);
-      ResultSet results = stmt.executeQuery();
+    String query = String.format(SEARCH_QUERY, surname);
+    try (Statement stmt = database.createStatement()) {
+      ResultSet results = stmt.executeQuery(query);
       while (results.next()) {
         Record rec = new Record();
         rec.setSurname(results.getString(2));
